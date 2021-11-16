2b57c29a956146faaf17caac1947c39dd79ec8fb
ssar@nate.com <ssar@nate.com>
2021-11-17 00:15:27 +0900

    8. 왼쪽벽과 오른쪽벽에 충돌했을 때 캐릭터 정지시키기

---
+++ src/bubble/test/ex07/BackgroundPlayerService.java
@@ -0,0 +1,55 @@
+package bubble.test.ex07;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+// 메인스레드 바쁨 - 키보드 이벤트를 처리하기 바쁨.
+// 백그라운드에서 계속 관찰
+public class BackgroundPlayerService implements Runnable{
+
+	private BufferedImage image;
+	private Player player;
+	
+	public BackgroundPlayerService(Player player) {
+		this.player = player;
+		try {
+			image = ImageIO.read(new File("image/backgroundMapService.png"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			// 색상 확인
+			Color leftColor = new Color(image.getRGB(player.getX() - 10, player.getY() + 25));
+			Color rightColor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
+
+			
+			if(leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+				System.out.println("왼쪽 벽에 충돌함");
+				player.setLeftWallCrash(true);
+				player.setLeft(false);
+			}else if(rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+				System.out.println("오른쪽 벽에 충돌함");
+				player.setRightWallCrash(true);
+				player.setRight(false);
+			}else {
+				player.setLeftWallCrash(false);
+				player.setRightWallCrash(false);
+			}
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/bubble/test/ex07/Moveable.java
@@ -0,0 +1,8 @@
+package bubble.test.ex07;
+
+public interface Moveable {
+	public abstract void left();
+	public abstract void right();
+	public abstract void up();
+	public abstract void down();
+}
