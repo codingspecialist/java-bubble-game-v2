61abf10e2e06c794f234d4062627d4ad71fb17a0
ssar@nate.com <ssar@nate.com>
2021-11-17 12:06:07 +0900

    11. 물방울 만들기

---
+++ src/bubble/test/ex10/Bubble.java
@@ -0,0 +1,57 @@
+package bubble.test.ex10;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Bubble extends JLabel{
+	
+	// 의존성 콤포지션
+	private Player player;
+	
+	// 위치 상태
+	private int x;
+	private int y;
+
+	// 움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	
+	// 적군을 맞춘 상태
+	private int state; // 0(물방울), 1(적을 가둔 물방울)
+	
+	private ImageIcon bubble; // 물방울
+	private ImageIcon bubbled; // 적을 가둔 물방울
+	private ImageIcon bomb; // 물방울이 터진 상태
+
+	public Bubble(Player player) {
+		this.player = player;
+		initObject();
+		initSetting();
+	}
+	
+	private void initObject() {
+		bubble = new ImageIcon("image/bubble.png");
+		bubbled = new ImageIcon("image/bubbled.png");
+		bomb = new ImageIcon("image/bomb.png");
+	}
+	
+	private void initSetting() {
+		left = false;
+		right = false;
+		up = false;
+		
+		x = player.getX();
+		y = player.getY();
+		
+		setIcon(bubble);
+		setSize(50, 50);
+		
+		state = 0;
+	}
+}
